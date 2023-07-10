d28934e66055dbe4598d9f076971255d01d34b84
ErenSahind <droppeed@gmail.com>
2023-07-10 13:45:22 +0300

    basic database added

--- odevler/todolist/app.js
+++ odevler/todolist/app.js
@@ -1,37 +1,112 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date=require(__dirname+"/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
-const items = ["Buy food", "Cook food", "Eat food"];
-const workItems=[];
+app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.set("view engine", "ejs");
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/todolistDB');
+    console.log("Başarıyla bağlandı.")
+}
 
-app.get("/", function (req, res) {
-    const day=date.getDate();
+main().catch(err => console.log(err));
 
-    res.render("list", { listTitle: day, newListItems: items });
-});
-
-app.post("/", function (req, res) {
-    const item = req.body.newItem;
-    if (req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect("/work");
-    } else {
-        items.push(item);
-        res.redirect("/");
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
     }
 });
 
-app.get("/work",function(req,res){
-    res.render("list",{listTitle:"Work List",newListItems:workItems});
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+// Item.insertMany(defaultItems).then(function () {
+//     console.log("Successfully saved all the items to todolistDB.");
+// }).catch(function (err) {
+//     console.log(err);
+// });
+
+// async function findItems() {
+//     try {
+//      console.log(await Item.find().exec())
+//     }
+//     catch (err) {
+//         console.error(err);
+//     }
+// }
+// findItems();
+
+app.get("/", function (req, res) {
+    async function findItems() {
+        try {
+            const foundItems = await Item.find().exec();
+            if (foundItems.length === 0) {
+                Item.insertMany(defaultItems).then(function () {
+                    console.log("Successfully saved all the items to todolistDB.");
+                }).catch(function (err) {
+                    console.log(err);
+                });
+                res.redirect("/")
+            } else {
+                res.render("list", { listTitle: "Today", newListItems: foundItems });
+            }
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+    findItems();
+});
+
+app.post("/", function (req, res) {
+    const itemName = req.body.newItem;
+    const item=new Item({
+        name:itemName
+    })
+    item.save();
+    res.redirect("/");
+});
+
+app.post("/delete",function(req,res){
+    const checkedItemId=req.body.checkbox;
+    async function deleteItem(){
+        try{
+            Item.deleteOne({_id:checkedItemId}).then(function(){
+                console.log("Deleted successfully.")
+            }).catch(function(err){
+                console.log(err);
+            })  
+        }
+        catch(err){
+            console.error(err);
+        }
+    }
+    deleteItem();
+    res.redirect("/");
 })
-app.get("/about",function(req,res){
+
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+})
+app.get("/about", function (req, res) {
     res.render("about");
 });
 app.listen(3000, function () {
