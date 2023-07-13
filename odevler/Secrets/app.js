9c16440891f2c0660ce469fd3d73a59cd5e6061d
ErenSahind <droppeed@gmail.com>
2023-07-13 13:49:48 +0300

    cookies and sessions using passport.js

--- odevler/Secrets/app.js
+++ odevler/Secrets/app.js
@@ -3,20 +3,32 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
 // const encrypt = require("mongoose-encryption")
 const app = express();
 const CryptoJS = require("crypto-js");
+const salt = CryptoJS.lib.WordArray.random(128 / 8);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/userDB');
     console.log("Başarıyla bağlandı.")
-}
+};
 
 main().catch(err => console.log(err));
 
@@ -25,36 +37,22 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 // userSchema.plugin(encrypt, { secret: process.env.SECRET_KEY, encryptedFields: ["password"] });
 
 
 
 const User = mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/", function (req, res) {
     res.render("home");
 });
-
-app.route("/login")
-    .get(function (req, res) {
-        res.render("login");
-    })
-    .post(function (req, res) {
-        const username = req.body.username;
-        const passwordHash = CryptoJS.SHA256(req.body.password).toString(CryptoJS.enc.Hex);
-        User.findOne({ email: username }).then(function (foundUser) {
-            if (foundUser.password === passwordHash) {
-                console.log("Giriş başarılı.");
-                res.render("secrets");
-            } else {
-                console.log("Şifre yanlış");
-                res.redirect("login");
-            }
-        }).catch(function () {
-            console.log("Kullanıcı bulunamadı");
-            res.redirect("login");
-        })
-    });
 
 app.route("/register")
 
@@ -63,25 +61,89 @@ app.route("/register")
     })
 
     .post(function (req, res) {
-        User.findOne({ email: req.body.username }).then(function (foundUserName) {
-            if (!foundUserName) {
-                const passwordHash = CryptoJS.SHA256(req.body.password).toString(CryptoJS.enc.Hex);
-                const newUser = new User({
-                    email: req.body.username,
-                    password: passwordHash
-                });
-                newUser.save().then(function () {
-                    res.render("secrets");
-                    console.log("New user added to the db.")
-                }).catch(function (err) {
-                    console.log(err);
-                })
+        User.register({ username: req.body.username }, req.body.password, function (err, user) {
+            if (err) {
+                console.log(err);
+                res.redirect("/register");
             } else {
-                console.log("Already taken.");
-                res.redirect("/");
+                passport.authenticate("local")(req, res, function () {
+                    res.redirect("/secrets");
+                })
+            }
+        });
+        // User.findOne({ email: req.body.username }).then(function (foundUserName) {
+        //     if (!foundUserName) {
+        //         const saltPw = (req.body.password).toString + salt.toString();
+        //         const passwordHash = CryptoJS.SHA256(saltPw).toString(CryptoJS.enc.Hex);
+        //         const newUser = new User({
+        //             email: req.body.username,
+        //             password: passwordHash
+        //         });
+        //         newUser.save().then(function () {
+        //             res.render("secrets");
+        //             console.log("New user added to the db.")
+        //         }).catch(function (err) {
+        //             console.log(err);
+        //         })
+        //     } else {
+        //         console.log("Already taken.");
+        //         res.redirect("/");
+        //     }
+        // })
+    });
+app.route("/login")
+    .get(function (req, res) {
+        res.render("login");
+    })
+    .post(function (req, res) {
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password
+        })
+        req.login(user, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                passport.authenticate("local", { failureRedirect: "/login", failureMessage: true })(req, res, function () {
+                    console.log("doğru");
+                    res.redirect("/secrets");
+                })
             }
         })
     });
+// const username = req.body.username;
+// const saltPw = (req.body.password).toString + salt.toString();
+// const passwordHash = CryptoJS.SHA256(saltPw).toString(CryptoJS.enc.Hex);
+// User.findOne({ email: username }).then(function (foundUser) {
+//     if (foundUser.password === passwordHash) {
+//         console.log("Giriş başarılı.");
+//         res.render("secrets");
+//     } else {
+//         console.log("Şifre yanlış");
+//         res.redirect("login");
+//     }
+// }).catch(function () {
+//     console.log("Kullanıcı bulunamadı");
+//     res.redirect("login");
+// })
+
+app.get("/secrets", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout",function (req,res){
+    req.logout(function(err){
+        if (err){
+            console.log(err);
+        } else {
+            res.redirect("/");
+        }
+    })
+});
 
 
 
